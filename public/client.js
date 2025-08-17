612ad2be962bf114ea01a57e7eb155106b54b056
Andres Montes <ingmontes21@gmail.com>
2025-08-16 19:09:26 -0500

    Add controls so the user can play from mobile

--- public/client.js
+++ public/client.js
@@ -24,6 +24,20 @@
     }
     });
 
+    // Handle mobile buttons
+document.getElementById("btn-up").addEventListener("click", () => {
+  socket.emit("move", "UP");
+});
+document.getElementById("btn-down").addEventListener("click", () => {
+  socket.emit("move", "DOWN");
+});
+document.getElementById("btn-left").addEventListener("click", () => {
+  socket.emit("move", "LEFT");
+});
+document.getElementById("btn-right").addEventListener("click", () => {
+  socket.emit("move", "RIGHT");
+});
+
     // Input
     const ARROWS = new Set(["ArrowUp","ArrowDown","ArrowLeft","ArrowRight"]);
     const KEY_TO_DIR = {
